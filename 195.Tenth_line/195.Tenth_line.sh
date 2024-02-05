b0cad30bf324323885f24c706342a3d34de6e39f
Matvey Guralskiy <mathewguralskiy@gmail.com>
2024-02-05 10:15:47 +0200

    Add Solution of 195

---
+++ 195.Tenth_line/195.Tenth_line.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#--------------LEET CODE---------------
+#195.Tenth_line
+
+#Solution of Matvey Guralskiy
+#https://github.com/MatveyGuralskiy
+
+# Read from the file file.txt and output the tenth line to stdout.
+sed -n "10p" file.txt
