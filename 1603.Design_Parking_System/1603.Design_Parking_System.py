9fcc1e599d894ecf7ea3fa45837b682d9f6cdeee
MatveyGuralskiy <mathewguralskiy@gmail.com>
2024-02-06 14:21:23 +0200

    Add 194 solution and edit 1603

--- 1603.Design_Parking_System/1603.Design_Parking_System.py
+++ 1603.Design_Parking_System/1603.Design_Parking_System.py
@@ -2,4 +2,12 @@
 #1603.Design Parking System
 
 #Solution of Matvey Guralskiy
-#https://github.com/MatveyGuralskiy
+#https://github.com/MatveyGuralskiy 
+class ParkingSystem:
+
+    def __init__(self, big: int, medium: int, small: int):
+        self.slots = [0, big, medium, small]
+
+    def addCar(self, car_type: int) -> bool:
+        self.slots[car_type] = count = max(self.slots[car_type] - 1, -1)
+        return count > -1
