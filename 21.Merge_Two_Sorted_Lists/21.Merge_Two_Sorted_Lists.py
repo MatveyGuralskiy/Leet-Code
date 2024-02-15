6042d7368427a9013c2e5af4f0954c753da4c4bd
MatveyGuralskiy <mathewguralskiy@gmail.com>
2024-02-15 08:43:26 +0200

    Add solution 21

---
+++ 21.Merge_Two_Sorted_Lists/21.Merge_Two_Sorted_Lists.py
@@ -0,0 +1,54 @@
+#--------------LEET CODE---------------
+#1. Two Sum
+
+#Solution of Matvey Guralskiy
+#https://github.com/MatveyGuralskiy
+
+"""
+You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list.
+
+ 
+
+Example 1:
+
+
+Input: list1 = [1,2,4], list2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+Example 2:
+
+Input: list1 = [], list2 = []
+Output: []
+Example 3:
+
+Input: list1 = [], list2 = [0]
+Output: [0]
+ 
+
+Constraints:
+
+The number of nodes in both lists is in the range [0, 50].
+-100 <= Node.val <= 100
+Both list1 and list2 are sorted in non-decreasing order.
+"""
+class Solution:
+    def mergeTwoLists(self, list1, list2):
+        cur=dummy=ListNode()
+
+        while list1 and list2:
+            if list1.val < list2.val:
+                cur.next = list1
+                list1, cur = list1.next, list1
+            else:
+                cur.next = list2
+                list2 = list2.next
+                cur = cur.next
+        if list1 is not None:
+            cur.next = list1
+        else:
+            cur.next = list2
+
+        return dummy.next
